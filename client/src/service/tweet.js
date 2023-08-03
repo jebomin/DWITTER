d6ffad2f55eecabcb582f88619fbeae0c2d94691
jebomin <jebomin0515@naver.com>
2023-08-03 16:36:46 +0900

    Front-end implementation with Tweet APIs

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,43 +1,64 @@
 export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
+  //아래에서 base url을 http 이런식으로 가져올 수도 있지만 우리는 constructor에서 baseurl을 외부로부터 가져올 것임
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
 
   async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
+    //전체 내용을 받아올 것인지 특정한 내용만 받아올 것인지 결정해야 해서 선언
+    let query = username ? `?username=${username}` : ""; //username이 전달되면 username이라는 쿼리를 만들고 전달되지 않으면 빈 문자열
+    //fetch를 사용해 서버에서 response를 가져옴(그래서 url을 전달해줌)
+    const response = await fetch(`${this.baseURL}/tweets${query}`, {
+      //옵션 정의
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    //받아온 데이터를 json으로 변환
+    const data = await response.json();
+    if (response.status !== 200) {
+      throw new Error(data.message); //성공하지 않아서 에러 던짐
+    }
+    return data; //다 완성된 되었다면(데이터를 다 받았다면) 데이터 return
   }
 
   async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-    this.tweets.push(tweet);
-    return tweet;
+    const response = await fetch(`${this.baseURL}/tweets/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        //object를 json형태로 변환
+        text,
+        username: "ellie",
+        name: "Ellie",
+      }),
+    });
+    const data = await response.json();
+    if (response.status !== 201) {
+      throw new Error(data.message); //에러~
+    }
+    return data;
   }
 
   async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status !== 204) {
+      throw new Error();
+    }
   }
 
   async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
-    if (!tweet) {
-      throw new Error('tweet not found!');
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+    const data = await response.json();
+    if (response.status !== 200) {
+      throw new Error(data.message);
     }
-    tweet.text = text;
-    return tweet;
+    return data;
   }
 }
