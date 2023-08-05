77a874de19f084aa83c53f7388d4ca3849353760
jebomin <jebomin0515@naver.com>
2023-08-05 21:25:39 +0900

    Create a Data Layer on a Server

---
+++ server/data/tweet.js
@@ -0,0 +1,54 @@
+//아직 데이터 베이스 구현을 안 배웠기에 배열 사용
+let tweets = [
+  {
+    id: "1",
+    text: "난 귀여워",
+    createdAt: Date.now().toString(),
+    name: "Bob",
+    username: "bob",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+  {
+    id: "2",
+    text: "안뇽:D",
+    createdAt: Date.now().toString(),
+    name: "Bomin",
+    username: "bomin",
+  },
+];
+
+export function getAll() {
+  return tweets;
+}
+
+export function getAllByUsername(username) {
+  return tweets.filter((tweet) => tweet.username === username);
+}
+
+export function getById(id) {
+  return tweets.find((tweet) => tweet.id === id);
+}
+
+export function create(text, name, username) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  return tweet;
+}
+
+export function update(id, text) {
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+  }
+  return tweet;
+}
+
+export function remove(id) {
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+}
